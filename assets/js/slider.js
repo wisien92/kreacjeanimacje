be9cb47cd0380367815bf90b0370f89ec77eb531
JakubWisniewski <jakub@actum.games>
2023-03-27 15:20:49 +0200

    fix prices

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -63,10 +63,10 @@ $("#birthday-range").on("input change", function(e) {
 			document.getElementById("birthday-price").innerHTML = "350 ZŁ";
 			break;
 		case '2':
-			document.getElementById("birthday-price").innerHTML = "450 ZŁ";
+			document.getElementById("birthday-price").innerHTML = "500 ZŁ";
 			break;
 		case '3':
-			document.getElementById("birthday-price").innerHTML = "600 ZŁ";
+			document.getElementById("birthday-price").innerHTML = "650 ZŁ";
 			break;
 		case '4':
 			document.getElementById("birthday-price").innerHTML = "Cena ustalana indywidualnie";
